aa64183b11546cfc086b78e2b3c4c981f6667c9a
oconer711 <bryanalanoca15@gmail.com>
2021-09-01 19:35:51 -0400

    cuarta modificacion

---
+++ tarjetasGraficas.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>HOMEPAGE</title>
+<link rel="stylesheet" href="">
+</head>
+<body>
+<center>
+    <img src="https://www.profesionalreview.com/wp-content/uploads/2018/03/Las-tarjetas-gr%C3%A1ficas-actuales-se-enfrentan-a-las-generaciones-anteriores-1280x720.jpg" alt="" class="imgg">
+    <img src="imagenes/fondo.jpg" alt="" class="imgg2">
+    <h1>PRODUCTOS</h1>
+
+</center>
+</body>
+</html>
---
+++ tecno_STORE.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="cssTecnoStore.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <header>
+      <audio autoplay="true">
+        <source
+          src="../Proyecto final/Algorithm (Alternate Reality Version).mp3"
+          type="audio/mp3"
+        />
+      </audio>
+
+      <h1>Bienvenidos a Tecno Store</h1>
+      <nav>
+        <ul id="menu">
+          <li>
+            <a href="">registrarse</a>
+            <ul>
+              <li><a href="">Iniciar sesion</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="">perifericos</a>
+            <ul>
+              <li><a href="tarjetasGraficas.php">Tarjetas graficas</a></li>
+              <li><a href="">Mouses</a></li>
+              <li><a href="">Monitores</a></li>
+              <li><a href="">Memorias RAM</a></li>
+              <li><a href="">Headsets</a></li>
+              <li><a href="">Teclados</a></li>
+              <li><a href="">Motherboard</a></li>
+            </ul>
+          </li>
+          <li><a href="">Acerca de nosotros</a></li>
+        </ul>
+      </nav>
+    </header>
+    <div class="banner"></div>
+    <footer class="pie">
+      <div style="text-align: center">
+        <table class="piepagina" style="margin: 0 auto">
+          <tr>
+            <br />
+            <td class="red1">
+              <a href=""
+                ><img
+                  src="https://image.flaticon.com/icons/png/512/124/124010.png"
+                  width="100px"
+              /></a>
+            </td>
+            <td class="red2">
+              <a href=""
+                ><img
+                  src="https://www.reasonwhy.es/sites/default/files/nuevo_icono_youtube_-_reasonwhy.png"
+                  width="130px"
+              /></a>
+            </td>
+            <td class="red3">
+              <a href=""
+                ><img
+                  src="https://cdn.pixabay.com/photo/2021/06/15/12/14/instagram-6338393_960_720.png"
+                  width="100px"
+              /></a>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div style="text-align: center" class="fin">
+        <a href="" style="text-decoration: none"
+          >Derechos reservados a Hacker Squad @</a
+        >
+      </div>
+    </footer>
+  </body>
+</html>
