a2afc3781ad92aeee26a2c55264a3d7e628b481f
ziosha <zioshadarkmetal@gmail.com>
2021-09-02 12:18:13 -0400

    css conect

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="cssTecnoStore.css" rel="stylesheet" type="text/css" />
+    <link href="public/css/cssTecnoStore.css" rel="stylesheet" type="text/css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
