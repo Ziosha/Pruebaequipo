0e7986abee960293ea7879c03f07c76987d7dbdf
Joacospre <penalozajoaco739@gmail.com>
2021-09-02 15:09:33 -0400

    cambios

---
+++ view/Home/mouses.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="../../public/css/cssmouses.css" rel="stylesheet" type="text/css" />
+<title>MOUSES</title>
+<link rel="stylesheet" href="">
+</head>
+
+<body>
+
+<section class="wrap-3d">
+  <div class="intro">
+  <a class="animated-border-button" href="http://localhost:8090/Pruebaequipo/">Inicio</a>
+<br>
+<br>
+    <h1>Mouses Gaiming</h1>
+    <p>Encuentre los mejores mouses, busque para su conveniencia o gusto</p>
+
+  </div>
+  
+  <div class="item-3d">
+    <span class="ground"></span>
+    <figure class="item-content group">
+      <div class="item-img">
+        <img src="https://resource.logitechg.com/w_659,c_limit,f_auto,q_auto,f_auto,dpr_2.0/d_transparent.gif/content/dam/gaming/en/products/g903-hero/g903-hero-desktop-nw.png?v=1" alt="" />
+      </div>
+      <figcaption class="item-caption">
+        <p>
+          <strong>Logitech G900</strong><br>
+          Botones programables<br>
+          Inalambrico<br>
+          DPI maximo de 12000<br>
+          1830 MHz
+        </p>
+        <p>
+          <strong>Lock</strong><br>
+          RGB tenue <br>
+          Ambidiestro
+        </p>
+      </figcaption>
+    </figure>
+  </div>
+  
+  <div class="item-3d">
+    <span class="ground"></span>
+    <figure class="item-content group">
+      <div class="item-img">
+        <img src="https://www.corsair.com/corsairmedia/sys_master/productcontent/CH-9000109-EU-CG_M65_RGB_K_03.png" alt="" />
+      </div>
+      <figcaption class="item-caption">
+        <p>
+          <strong>Corsair M65 Pro RGB</strong><br>
+          8 botones programables<br>
+          no Inalambrico<br>
+          DPI maximo 12000<br>
+        </p>
+        <p>
+          <strong>lock</strong><br>
+          RGB potente <br>
+          Calidad inigualabre
+        </p>
+      </figcaption>
+    </figure>
+  </div>
+  
+  <div class="item-3d">
+    <span class="ground"></span>
+    <figure class="item-content group">
+      <div class="item-img">
+        <img src="https://assets.razerzone.com/eeimages/products/25919/daelite_gallery03.png" alt="" />
+      </div>
+      <figcaption class="item-caption">
+        <p>
+        <strong>Razer DeathAdder Elite</strong><br>
+          7 botones programables<br>
+          No es inlambrico<br>
+          DPI maximo 16000<br>
+          105 gramos
+        </p>
+        <p>
+          <strong>lock</strong><br>
+          RGB tenue<br>
+          50 millones de clicks
+        </p>
+      </figcaption>
+    </figure>
+  </div>
+  
+  <div class="item-3d">
+    <span class="ground"></span>
+    <figure class="item-content group">
+      <div class="item-img">
+        <img src="https://www.cclonline.com/images/avante/Rival710_Persp2.png?width=530&height=400&scale=canvas" alt="" />
+      </div>
+      <figcaption class="item-caption">
+        <p>
+          <strong>SteelSeries Rival 710</strong><br>
+          7 botones programables <br>
+          No es inlambrico<br>
+          DPI maximo 16000<br>
+          
+        </p>
+        <p>
+          <strong>lock</strong><br>
+          RGB tenue<br>
+        </p>
+      </figcaption>
+    </figure>
+  </div>
+  
+  <div class="item-3d">
+    <span class="ground"></span>
+    <figure class="item-content group">
+      <div class="item-img">
+        <img src="https://www.corsair.com/corsairmedia/sys_master/productcontent/press_customization-leveled-up-corsair-launches-scimitar-pro-rgb-gaming-mouse-at-ces-2017.png" alt="" />
+      </div>
+      <figcaption class="item-caption">
+        <p>
+          <strong>Corsair Scimitar RGB</strong><br>
+          14 Botones programables<br>
+          No es inalambrico <br>
+          DPI maximo 16000<br>
+          8K
+        </p>
+        <p>
+          <strong>lock</strong><br>
+          RGB Activo potente<br>
+          Buen agarre y comodo de sujetar
+        </p>
+      </figcaption>
+    </figure>
+  </div>
+  
+  <footer class="footer">
+    <p>hacker squad &copy; 2021 hacker squad <a  target="_blank">Mouses</a></p>
+  </footer>
+</section>
+</body>
+</html>
