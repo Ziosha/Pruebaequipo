ec1f3354acfd2b7891077d9911ec3608df56b4a5
ziosha <zioshadarkmetal@gmail.com>
2021-09-02 22:39:33 -0400

    conflict

--- public/js/register.js
+++ public/js/register.js
@@ -11,26 +11,15 @@ firebase.initializeApp(config);
  
 
 var auth = firebase.auth();
-<<<<<<< HEAD
 document.getElementById('btlregis').addEventListener('click', function () {
-=======
-document.getElementById('btllogin').addEventListener('click', function () {
->>>>>>> patata
     var provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider)
     .then(function (result) {
         console.log(result.user.providerData[0].displayName);
-<<<<<<< HEAD
         console.log(result.user.providerData[0].uid);
         console.log(result.user.providerData[0].photoURL);
 
-        $.post("../../controller/usuario.php?op=registro",{nomUsu:result.user.providerData[0].displayName,pass:123456,correo:result.user.providerData[0].uid},function(data){
-=======
-        console.log(result.user.providerData[0].email);
-        console.log(result.user.providerData[0].photoURL);
-
-        $.post("controller/usuario.php?op=registro",{nomUsu:result.user.providerData[0].displayName,correo:result.user.providerData[0].email,pass:123456},function(data){
->>>>>>> patata
+            $.post("../../controller/usuario.php?op=registro",{nomUsu:result.user.providerData[0].displayName,pass:123456,correo:result.user.providerData[0].uid},function(data){
             if(data==0){
                 Swal.fire({
                     icon: 'success',
@@ -39,13 +28,8 @@ document.getElementById('btllogin').addEventListener('click', function () {
                     confirmButtonText: `Ok`,
                 }).then((result) => {
                     if (result.isConfirmed) {
-<<<<<<< HEAD
                         window.open('../../index.php','_self');
-=======
-                        window.open('http://localhost/login/view/home/','_self');
->>>>>>> patata
-                    }
-                });
+                    })
             }else{
                 Swal.fire(
                     'Observacion!',
@@ -69,11 +53,10 @@ $(document).ready(function() {
 
 });
 
-<<<<<<< HEAD
+
 $(document).on("click", "#btnregistrar", function () {
-=======
-$(document).on("click", "#btnregister", function () {
->>>>>>> patata
+
+
     var nomUsu =  $('#nomUsu').val();
     var correo =  $('#correo').val();
     var pass =  $('#pass').val();
@@ -87,11 +70,10 @@ $(document).on("click", "#btnregister", function () {
         );
     }else{
         if (pass==pass1){
-<<<<<<< HEAD
+
             $.post("../../controller/usuario.php?op=registro",{nomUsu:nomUsu,pass:pass,correo:correo},function(data){
-=======
-            $.post("controller/usuario.php?op=registro",{nomUsu:nomUsu,correo:correo,pass:pass},function(data){
->>>>>>> patata
+
+
                 if(data==0){
                     Swal.fire({
                         icon: 'success',
@@ -100,11 +82,9 @@ $(document).on("click", "#btnregister", function () {
                         confirmButtonText: `Ok`,
                     }).then((result) => {
                         if (result.isConfirmed) {
-<<<<<<< HEAD
+
                             window.open('../../index.php','_self');
-=======
-                            window.open('http://localhost/login/view/home/','_self');
->>>>>>> patata
+
                         }
                     });
                 }else{
